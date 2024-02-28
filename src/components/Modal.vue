5879b9dc7f96c273c09597496954ea10455867b9
austriker27 <davidalindahl@gmail.com>
2024-02-28 09:04:31 -0800

    Improve modal responsiveness

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white p-8 md:p-14 lg:py-[4.5rem] lg:pr-[4.5rem] lg:pl-16 relative max-w-[36rem]">
+  <div class="bg-white p-6 md:p-14 lg:py-[4.5rem] lg:pr-[4.5rem] lg:pl-16 relative 2xl:max-w-[40rem]">
       <button @click="$emit('closeModal')" aria-label="Close Modal" class="absolute top-4 right-4 transition-opacity duration-200 ease-out hover:opacity-80 focus-visible:ring-4 focus-visible:ring-red-900 focus-visible:outline-none">
         <XMark class="w-4 h-4" />
       </button>
