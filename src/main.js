ae07084bd5bd89abbb0b3a56653be977db746d92
austriker27 <davidalindahl@gmail.com>
2024-02-28 09:03:29 -0800

    Rmove unused packages like sal and tw debug

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import sal from 'sal.js';
 
 createApp(App).mount('#app')
 
 // FONTS:
 import '@fontsource/gfs-didot';
 import '@fontsource/lato';
-
-
-sal();
