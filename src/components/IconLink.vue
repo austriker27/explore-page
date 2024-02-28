b5fff54eec231b444866734d61b68f9fa5daf728
austriker27 <davidalindahl@gmail.com>
2024-02-28 10:12:45 -0800

    Add aria label to icon link to improve a11y

--- src/components/IconLink.vue
+++ src/components/IconLink.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <a :href="url" target="_blank" class="shadow-sm hover:shadow-lg transition-all duration-200 ease-out hover:opacity-80  focus-visible:ring-red-900 focus-visible:outline-none focus-visible:ring-4">
+  <a :aria-label="type + ' link'" :href="url" target="_blank" class="shadow-sm hover:shadow-lg transition-all duration-200 ease-out hover:opacity-80  focus-visible:ring-red-900 focus-visible:outline-none focus-visible:ring-4">
     <Facebook v-if="type === 'Facebook'" class="w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16" />
     <Instagram v-if="type === 'Instagram'" class="w-8 h-8 sm:w-12 sm:h-12 lg:w-16 lg:h-16" />
   </a>
