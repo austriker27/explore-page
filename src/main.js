e1be75a40429584bda3184e9e463714f8de502ba
austriker27 <davidalindahl@gmail.com>
2024-02-27 16:11:16 -0800

    Adds sal

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import sal from 'sal.js';
 
 createApp(App).mount('#app')
 
+// FONTS:
 import '@fontsource/gfs-didot';
 import '@fontsource/lato';
+
+
+sal();
